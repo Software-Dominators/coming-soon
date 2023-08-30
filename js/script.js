c7d7fcbb1cd671155f65600b52dd25078fce803f
= <=>
2023-08-30 16:23:40 +0300

    coming-soon-landing-page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+	
+        $(document).ready(function (){
+			
+			var myDate = new Date();
+			myDate.setDate(myDate.getDate() +10);
+            $("#countdown").countdown(myDate, function (event) {
+                $(this).html(
+                    event.strftime(
+                        '<div class="timer-wrapper"><div class="time">%D</div><span class="text">Days</span></div><div class="timer-wrapper"><div class="time">%H</div><span class="text">Hours</span></div><div class="timer-wrapper"><div class="time">%M</div><span class="text">Minutes</span></div><div class="timer-wrapper"><div class="time">%S</div><span class="text">Seconds</span></div>'
+                    )
+                );
+            });
+
+        });
+
+	
