483a5e4f74fbf024158d1d537deb30eba98233f1
= <=>
2023-08-30 20:57:44 +0300

    timer update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
 
-	
-        $(document).ready(function (){
-			
-			var myDate = new Date();
-			myDate.setDate(myDate.getDate() +10);
-            $("#countdown").countdown(myDate, function (event) {
-                $(this).html(
-                    event.strftime(
-                        '<div class="timer-wrapper"><div class="time">%D</div><span class="text">Days</span></div><div class="timer-wrapper"><div class="time">%H</div><span class="text">Hours</span></div><div class="timer-wrapper"><div class="time">%M</div><span class="text">Minutes</span></div><div class="timer-wrapper"><div class="time">%S</div><span class="text">Seconds</span></div>'
-                    )
-                );
-            });
+	// The End Of The Year Date To Countdown To
+// 1000 milliseconds = 1 Second
 
-        });
+let countDownDate = new Date("Sep 25, 2023 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+        // $(document).ready(function (){
+			
+		// 	var myDate = new Date();
+		// 	myDate.setDate(myDate.getDate() +10);
+        //     $("#countdown").countdown(myDate, function (event) {
+        //         $(this).html(
+        //             event.strftime(
+        //                 '<div class="timer-wrapper"><div class="time">%D</div><span class="text">Days</span></div><div class="timer-wrapper"><div class="time">%H</div><span class="text">Hours</span></div><div class="timer-wrapper"><div class="time">%M</div><span class="text">Minutes</span></div><div class="timer-wrapper"><div class="time">%S</div><span class="text">Seconds</span></div>'
+        //             )
+        //         );
+        //     });
+
+        // });
 
 	
